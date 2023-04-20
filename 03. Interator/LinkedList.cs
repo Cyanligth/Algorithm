8c3aca555cb7cface75d50dd9cc4399dd5cee615
Cyanligth <129037620+Cyanligth@users.noreply.github.com>
2023-04-20 20:59:59 +0900

    반복기

---
+++ 03. Interator/LinkedList.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    public class LinkedListNode<T>
+    {
+        internal LinkedList<T> linkList;        // 연결리스트의 링크리스트 변수
+        internal LinkedListNode<T> next;        // 노드의 넥스트 변수
+        internal LinkedListNode<T> prev;        // 노드의 프리브 변수
+        private T item;     // 일반화 아이템 변수
+
+        public LinkedList<T> List { get { return linkList; } }      // 리스트 함수, 호출되면 링크리스트 반환
+        public LinkedListNode<T> Prev { get { return prev; } }      // 프리브 함수, 호출되면 프리브 반환
+        public LinkedListNode<T> Next { get { return next; } }      // 넥스트 함수, 호출되면 넥스트 반환
+        public T Value { get { return item; } set { item = value; } }       // 일반화 벨류 함수, 호출되면 아이템을 반환하고 아이템에 받아온 값을 저장
+
+        public LinkedListNode(T value)      // 초기화
+        {
+            this.linkList = null;
+            this.prev = null;
+            this.next = null;
+            this.item = value;
+        }
+
+        public LinkedListNode(LinkedList<T> linkList, T value)      // 오버로딩
+        {
+            this.linkList = linkList;
+            this.prev = null;
+            this.next = null;
+            this.item = value;
+        }
+
+        public LinkedListNode(LinkedList<T> linkList, LinkedListNode<T> prev, LinkedListNode<T> next, T value)      // 오버로딩22
+        {
+            this.linkList = linkList;
+            this.prev = prev;
+            this.next = next;
+            this.item = value;
+        }
+    }
+
+    public class LinkedList<T> : IEnumerable<T>
+    {
+        private LinkedListNode<T> head;     // 머리 변수, 연결리스트 제일 앞 주소 저장
+        private LinkedListNode<T> tail;     // 꼬리 변수, 연결리스트 제일 끝 주소 저장
+        private int count;      // 카운트 변수. 현재 연결리스트에 몇개나 들어잇는지 체크
+
+        public LinkedList()     // 초기화
+        {
+            this.head = null;
+            this.tail = null;
+            this.count = 0;
+        }
+
+        public LinkedListNode<T> First { get { return head; } }     // 퍼스트 함수, 호출되면 머리 반환
+        public LinkedListNode<T> Last { get { return tail; } }      // 라스트 함수, 호출되면 꼬리 반환
+        public int Count { get { return count; } }      // 카운트 함수, 호출되면 카운트 반환
+
+        public LinkedListNode<T> AddFirst(T value)      // 제일 앞에 끼워넣기
+        {
+            LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);     // 뉴노드 변수에 받아온 값 저장
+            if (head != null)    // 머리가 공백이 아닌 경우
+            {
+                head.prev = newNode;        // 머리의 이전 노드가 뉴노드를 가리킨다
+                newNode.next = head;        // 뉴노드의 다음 노드가 머리를 가리킨다
+                head = newNode;     // 지금부터 뉴노드가 머리다.
+            }
+            else        // 그 외의 경우
+            {
+                head = newNode;     // 뉴노드가 머리이자
+                tail = newNode;     // 동시에 꼬리이다
+            }
+            count++;        // 카운트 +1
+            return newNode;     // 뉴노드 반환
+        }
+
+        public LinkedListNode<T> AddLast(T value)       // 제일 뒤에 끼워넣기
+        {
+            LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);     // 뉴노드 변수에 받아온 값 저장
+            if (tail != null)       // 머리가 공백이 아닌 경우
+            {
+                tail.next = newNode;        // 꼬리의 다음 노드가 뉴노드를 가리킨다
+                newNode.prev = tail;        // 뉴노드의 이전 노드가 꼬리를 가리킨다
+                tail = newNode;     // 지금부터 뉴노드가 꼬리다
+            }
+            else        // 그 외의 경우
+            {
+                head = newNode;     // 뉴노드가 머리이자
+                tail = newNode;     // 동시에 꼬리이다
+            }
+            count++;        // 카운트 +1
+            return newNode;     // 뉴노드 반환
+        }
+
+        public LinkedListNode<T> AddBefore(LinkedListNode<T> node, T value)     // 지정된 노드의 앞에 끼워넣기
+        {
+            if (node.linkList != this)      // 지정된 노드가 다른 연결리스트에 속해있다면
+                throw new InvalidOperationException("node");        // 오류반환
+            if (node == null)       // 지정된 노드의 값이 없으면
+                throw new ArgumentNullException(nameof(node));      // 오류반환
+            LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);     // 뉴노드 선언하고 받아온 값 저장
+            newNode.next = node;        //  뉴노드의 다음은 지정된 노드를 가리킨다
+            newNode.prev = node.prev;       // 뉴노드의 이전은 지정된 노드가 이전으로 가리키던 노드를 가리킨다
+            node.prev = newNode;        // 지정된 노드의 이전은 뉴노드를 가리킨다
+            if (node.prev != null)      // 지정된 노드의 이전이 가리키던 것이 공백이 아니라면
+                node.prev.next = newNode;       // 지정된 노드의 이전이 가리키던 노드의 다음은 뉴노드를 가리킨다
+            else        // 그 외의 경우
+                head = newNode;     // 뉴노드는 지금부터 머리다.
+            count++;        // 카운트+1
+            return newNode;     // 뉴노드 반환
+        }
+
+        public LinkedListNode<T> AddAfter(LinkedListNode<T> node, T value)     // 지정된 노드의 앞에 끼워넣기
+        {
+            if (node.linkList != this)      // 지정된 노드가 다른 연결리스트에 속해있다면
+                throw new InvalidOperationException("node");        // 오류반환
+            if (node == null)       // 지정된 노드의 값이 없으면
+                throw new ArgumentNullException(nameof(node));      // 오류반환
+            LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);     // 뉴노드 선언하고 받아온 값 저장
+            newNode.prev = node;        //  뉴노드의 이전은 지정된 노드를 가리킨다
+            newNode.next = node.next;       // 뉴노드의 다음은 지정된 노드가 다음으로 가리키던 노드를 가리킨다
+            node.next = newNode;        // 지정된 노드의 다음은 뉴노드를 가리킨다
+            if (node.next != null)      // 지정된 노드의 다음이 가리키던 것이 공백이 아니라면
+                node.next.prev = newNode;       // 지정된 노드의 다음이 가리키던 노드의 이전은 뉴노드를 가리킨다
+            else        // 그 외의 경우
+                tail = newNode;     // 뉴노드는 지금부터 꼬리다.
+            count++;        // 카운트+1
+            return newNode;     // 뉴노드 반환
+        }
+
+        public void Remove(LinkedListNode<T> node)      // 받아온 값을 연결리스트에서 지우기
+        {
+            if (node.linkList != this)      // 받아온 값이 현재 연결리스트에 없는 경우
+                throw new InvalidOperationException("node");        // 오류 반환
+            if (node == null)       // 받아온 값이 공백이면
+                throw new ArgumentNullException(nameof(node));      // 오류 반환
+
+            if (node == head)       // 받아온 값이 머리라면
+
+                if (node == tail)       // 받아온 값이 꼬리라면
+
+
+                    if (node.prev != null)      // 받아온 노드의 이전이 공백을 가리키고 있지 않다면 == 머리가 아니라면
+                        node.prev.next = node.next;     // 받아온 노드의 이전이 가리키는 노드의 다음은 노드의 다음이 가리키는 주소를 가리킨다.
+                    else        // 그 외의 경우 == 받아온 값이 머리면
+                        head = node.next;       // 다음 노드가 머리가 된다
+            if (node.next != null)      // 받아온 노드의 다음이 공백을 가리키고 있지 않다면 == 꼬리가 아니라면
+                node.next.prev = node.prev;     // 받아온 노드의 다음이 가리키는 노드의 이전은 노드의 이전이 가리키는 주소를 가리킨다.
+            else        // 그 외의 경우 == 받아온 값이 꼬리면
+                tail = node.prev;       // 이전 노드가 꼬리가 된다
+            count--;        // 카운트 -1
+        }
+
+        public bool Remove(T value)     // 지우기
+        {
+            LinkedListNode<T> find = Find(value);       // 찾기 함수 호출해서 파인드 변수에 결과 저장
+            if (find != null)       // 파인드가 공백이 아니라면 == 찾기 함수에서 뭔가를 찾았으면
+            {
+                Remove(find);       // 이거 말고 위에 지우기 함수에 파인드 넣어서 지운다 
+                return true;        // 참 반환 == 지우는거 성공
+            }
+            else return false;      // 거짓 반환 == 못지움
+        }
+
+        public void RemoveFirst()       // 연결리스트 머리 지우기
+        {
+            if (head == null && tail == null)       // 머리랑 꼬리가 둘 다 공백이면 == 리스트가 비어있으면
+                throw new InvalidOperationException();      // 오류반환
+            head.next.prev = null;      // 머리의 다음이 가리키는 노드의 이전이 공백을 가리키게 한다
+            head = head.next;       // 머리의 다음이 가리키는 노드를 머리로 설정
+            count--;        // 카운트 -1
+        }
+
+        public void RemoveLast()        // 연결리스트 꼬리 지우기
+        {
+            if (head == null && tail == null)       // 머리랑 꼬리가 둘 다 공백이면 == 리스트가 비어있으면
+                throw new InvalidOperationException();      // 오류반환
+            tail.prev.next = null;      // 꼬리의 이전이 가리키는 노드의 다음이 공백을 가리키게 한다
+            tail = tail.prev;       // 머리의 이전이 가리키는 노드를 꼬리로 설정
+            count--;        // 카운트 -1
+        }
+
+        public LinkedListNode<T> Find(T value)      // 연결리스트에서 받아온 값 찾기
+        {
+            LinkedListNode<T> target = head;        // 시작부분을 머리로 정한다.
+            EqualityComparer<T> comparer = EqualityComparer<T>.Default;     // 일반화 형식 비교를 위한 무언가
+            while (target != null)      // 타겟이 공백이 될때까지 반복
+            {
+                if (comparer.Equals(value, target.Value))   // 현재 값(시작은 머리)과 받아온 값을 비교한 결과가 참이라면
+                    return target;      // 현재 값 반환
+                target = target.next;       // 타겟을 타겟의 다음이 가리키는 값으로 바꾼다.
+            }
+            return null;        // 똑같은걸 한번도 못찾으면 공백 반환
+        }
+
+        public LinkedListNode<T> FindLast(T value)      // 연결리스트에서 받아온 값 찾기
+        {
+            LinkedListNode<T> target = tail;        // 시작부분을 꼬리로 정한다.
+            EqualityComparer<T> comparer = EqualityComparer<T>.Default;     // 일반화 형식 비교를 위한 무언가
+            while (target != null)      // 타겟이 공백이 될때까지 반복
+            {
+                if (comparer.Equals(value, target.Value))   // 현재 값(시작은 꼬리)과 받아온 값을 비교한 결과가 참이라면
+                    return target;      // 현재 값 반환
+                target = target.prev;       // 타겟을 타겟의 이전이 가리키는 값으로 바꾼다.
+            }
+            return null;        // 똑같은걸 한번도 못찾으면 공백 반환
+        }
+
+        public void Clear()     // 연결리스트 초기화
+        {
+            head = null;        // 머리주소를 공백으로
+            tail = null;        // 꼬리주소를 공백으로
+            count = 0;      // 카운트를 0으로
+        } // 결과 = 연결리스트를 가리키는 포인터가 안남아서 GC가 맛있게 먹어버립니다
+
+        public bool Contains(T value)       // 받아온 값을 연결리스트가 포함하고 있는지 참/거짓으로 반환
+        {
+            if (Find(value) != null)        // 찾기 함수 호출, 결과가 공백이 아니라면 == 같은걸 찾았으면
+                return true;        // 참 반환
+            return false;       // 못찾으면 거짓 반환
+        }
+
+        // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+
+        public IEnumerator<T> GetEnumerator()       // 반복기 호출 함수
+        {
+            return new Enumerator(this);        // 반복기 새로 만들어서 반환
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()     // 반복기 호출 오버로딩
+        {
+            return new Enumerator(this);        // 반복기 새로 만들어서 반환
+        }
+
+        public struct Enumerator : IEnumerator<T>       // 반복기 구조체 선언
+        {
+            private LinkedList<T> linkList;     // 연결리스트형 변수
+            private LinkedListNode<T> head;     // 연결리스트노드형 변수
+            private T current;      // 일반화 현재값 변수
+
+            internal Enumerator(LinkedList<T> linkList)     // 초기화
+            {
+                this.linkList = linkList;
+                this.head = linkList.head;
+                this.current = default(T);
+            }
+
+            public T Current { get { return current; } }        // 호출되면 현재값 반환하는 함수
+
+            object IEnumerator.Current { get { return current; } }      
+            // 오브젝트형 반복기의 현재값. 리스트랑 다르게 인덱스가 없기에 오류확인 if문을 안거친다. 호출되면 현재값 반환.
+
+            public void Dispose() { }       // 리스트떄랑 이하동문
+
+            public bool MoveNext()      // 무브넥스트 함수, 반복기의 위치를 옮겨주고 참/거짓을 반환
+            {
+                if (head != null)       // 만약 머리가 없지 않다면 == 있으면
+                {
+                    current = head.Value;       // 현재값은 머리의 값이 된다. -중요, 주소가 아니라 값이다.
+                    head = head.next;       // 머리의 다음을 머리로 정한다. - 여기서 머리는 그냥 변수 이름이다. 연결리스트의 머리를 뜻하지 않는다.
+                    return true;        // 참 반환
+                }
+                else        // 그 외의 경우 == 머리가 없으면
+                {
+                    current = default(T);       // 현재값은 기본값이 된다
+                    return false;       // 거짓 반환
+                }
+            }
+
+            public void Reset()     // 초기화 함수
+            {
+                this.head = linkList.head;      // 머리변수가 가리키는 주소가 연결리스트의 제일 앞이 된다. 
+                current = default(T);       // 현재값을 공백으로
+            }
+        }
+        public int Average(LinkedList<int> linkList)
+        {
+            int sum = 0;
+            int ave = 0;
+            foreach (int i in linkList) { sum = sum + i; }
+            ave = sum/linkList.Count;
+            return ave;
+        }
+    }
+}
---
+++ 03. Interator/List.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    public class List<T> : IEnumerable<T>, IList<T>
+    {
+        private const int baseCount = 20;       // 리스트 기본 길이를 20으로 지정
+        private T[] array;      // 리스트로 미리 정할 배열 선언
+        private int size;       // 정수형 크기 변수 선언
+        public int Count { get { return size; } }       // 카운트 함수 선언, 현재 사용되고 있는 배열의 길이를 받아온다.
+        public int Capacity { get { return array.Length; } }        // 카파시티 함수 선언, 배열의 총 길이를 가져온다.
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public List()     // 초기화
+        {
+            this.array = new T[baseCount];      // 어레이 배열을 일반화 형식의 베이스 카운트 길이를 가진 배열로 초기화
+            this.size = 0;                      // 기본 길이를 0으로 초기화
+        }
+
+        public T this[int index]        // 인덱서
+        {
+            get
+            {
+                if (index < 0 || index >= Count) { throw new ArgumentOutOfRangeException("index"); }
+                // 인덱스가 0보다 작거나 사용되고 있는 배열의 길이보다 크거나 같다면 범위를 벗어남 오류 반환
+                return array[index];    // 인덱스 번째의 배열값 반환
+            }
+            set
+            {
+                if (index < 0 || index >= Count) { throw new ArgumentOutOfRangeException("index"); }
+                // 인덱스가 0보다 작거나 사용되고 있는 배열의 길이보다 크거나 같다면 범위를 벗어남 오류 반환
+                array[index] = value;   // 배열의 인덱스번째에 받아온 값을 저장한다.
+            }
+        }
+
+        public void Add(T array)    // 배열에 값을 저장하는 함수
+        {
+            if (size < baseCount)   // 사용되고 있는 배열의 길이가 배열의 총 길이보다 작다면
+            {
+                this.array[size++] = array;     // 받아온 값을 사이즈 번째의 배열에 저장하고 사이즈를 1 올린다.
+            }
+            else        // 그 외의 경우(사용되고 있는 배열의 길이가 배열의 총 길이를 넘었을 경우)
+            {
+                Grow();     // 그로우 함수 호출 - 배열의 길이를 늘린다
+                this.array[size++] = array;     // 받아온 값을 사이즈 번째의 배열에 저장하고 사이즈를 1 올린다.
+            }
+        }
+
+        public bool Remove(T array)     // 참 거짓의 값을 반환하는 리무브 변수. 값을 제거하고 싶을떄 이게 호출된다.
+        {
+            int index = IndexOf(array);     // 인덱스오브 함수에 받아온 값을 넣어 반환받은 값을 정수형 인덱스 변수에 저장
+            if (index >= 0)     // 인덱스가 0보다 크거나 같을 경우
+            {
+                RemoveAt(index);        // 리무브엣 함수에 인덱스 값을 넣어 실행하고 참 반환
+                return true;
+            }
+            return false;       // 거짓 반환
+        }
+
+        public void RemoveAt(int index)     // 리무브엣 함수. 배열에서 값을 직접 제거하는 함수다.
+        {
+            if (index < 0 || index >= Count) { throw new ArgumentOutOfRangeException("index"); }
+            // 받아온 인덱스가 0보다 작거나 사용되고 있는 배열의 길이보다 크거나 같다면 범위를 벗어남 오류 반환
+            size--;     // 배열의 길이를 하나 줄인다.
+            Array.Copy(array, index + 1, array, index, size - index);
+            // 제거하기 원하는 값의 인덱스를 받아왔기에 기존 배열에서 해당 인덱스에서 1을 더한 부분부터 배열의 끝까지를
+            // 인덱스부터 덮어씌운다 = 한칸씩 앞으로 당긴다.
+        }
+
+        public int IndexOf(T array)     // 인덱스오브 함수. 원하는 값의 위치를 찾는다.
+        {
+            return Array.IndexOf<T>(this.array, array, 0, size);    // 받아온 값이 배열의 몇번째에 있는지 찾아서 반환.
+        }
+
+        public void Clear()     // 리스트를 초기화하는 함수
+        {
+            array = new T[baseCount];       // 배열의 주소를 새로 만들어서 기존 배열은 가비지 컬렉터가 먹어버리게 한다.
+            size = 0;
+        }
+
+        public void Grow()      // 배열의 길이를 늘리는 함수. 사실 늘리는게 아니라 새로 만드는거다.
+        {
+            int newLength = array.Length * 2;      // 새오운 배열 길이를 현재 배열의 길이의 2배로 정한다.
+            T[] newArray = new T[newLength];        // 일반화 형식의 새로운 배열 선언. 길이는 위에서 정한대로 기존의 2배.
+            Array.Copy(array, newArray, size);      // 기존 배열의 값을 새로운 배열로 복사한다
+            array = newArray;       // 기존 배열의 주소를 새로운 배열로 바꾼다
+        }
+
+        public T? Find(Predicate<T> match)      // 해당 값이 배열에 존재하는지 찾는다.
+        {
+            if (match == null) { throw new ArgumentNullException("match"); }
+            // 받아온 값이 비어있으면 공백 오류 반환
+            for (int i = 0; i < size; i++)       // 현재 사용되는 배열의 길이만큼 반복
+            {
+                if (match(array[i])) { return array[i]; }
+                // 배열의 i번째 값이 받아온 값과 같다면 배열의 i번째 값을 반환
+            }
+            return default(T);      // 그렇지 않다면 기본값 반환
+        }
+
+        public int FindIndex(Predicate<T> match)        // 해당 값이 배열의 몇번째 위치에 있는지 찾는다.
+        {
+            if (match == null) { throw new ArgumentNullException("match"); }
+            // 받아온 값이 비어있으면 공백 오류 반환
+            for (int i = 0; i < size; i++)      // 현재 사용되는 배열의 길이만큼 반복
+            {
+                if (match(array[i])) { return i; }
+                // 배열의 i번째 값이 받아온 값과 같다면 i를 반환
+            }
+            return -1;      // 그렇지 않다면 -1 반환
+        }
+
+        public bool Contains(T array)       // 해당 값이 배열에 존재하는지 찾아서 참/거짓으로 반환
+        {
+            if (IndexOf(array) > -1)      // 인덱스오브 함수 호출(해당 값이 배열의 몇번째에 있는지 반환)
+            {
+                return true;        // 참이라면 참 반환
+            }
+            else { return false; }      // 그 외의 경우 거짓 반환
+        }
+
+        public void Insert(int index, T array)      // 인덱스번쨰의 배열에 받아온 값을 넣는다
+        {
+            this.array[index] = array;      // 위에 쓴 그대로 썼어요
+        }
+
+        public void CopyTo(T[] array)    // 리스트를 1차원 배열에 복사
+        {
+            Array.Copy(this.array, array, 0);
+        }
+
+        // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            Array.Copy(this.array, array, arrayIndex);
+        }
+
+        public IEnumerator<T> GetEnumerator()       // 반복기 호출 함수
+        {
+            return new Enumerator(this);        // 반복기 새로 만들어서 반환
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()     // 반복기 호출 오버로딩
+        {
+            return new Enumerator(this);        // 반복기 새로 만들어서 반환
+        }
+
+        public struct Enumerator : IEnumerator<T>       // 반복기 구조체 선언
+        {
+            private List<T> list;       // 리스트 형식 리스트 변수
+            private int index;      // 정수형 인덱스
+            private T current;      // 일반화 현재값
+
+            internal Enumerator(List<T> list)       // 초기화
+            {
+                this.list = list;       
+                this.index = 0;
+                this.current = default(T);      
+            }
+
+            public T Current { get { return current; } }        // 호출되면 현재값을 반환하는 일반화 커런트 함수
+
+            object IEnumerator.Current      // 오브젝트형 반복기의 현재값
+            {
+                get 
+                {
+                    if (index < 0 || index >= list.Count)       // 만약 반복기의 위치가 0보다 작거나 사용되고 있는 리스트의 길이보다 같거나 더 멀리에 있는 경우
+                        throw new InvalidOperationException();      // 에러 반환
+                    return Current;     // 커런트 함수 호출 = 현재값 반환
+                }
+            }
+
+            public void Dispose() { }   // foreach문 종료시에 호출되는거. 대충 마무리 함수? 특별히 필요는 없기에 비워둔다.
+
+            public bool MoveNext()      // 무브넥스트 함수, 반복기의 위치를 옮겨주고 참/거짓을 반환한다.
+            {
+                if (index < list.Count)     // 반복기의 위치가 사용되고 있는 리스트의 길이보다 작은 경우
+                {
+                    current = list[index++];        // 현재값은 리스트의 인덱스 번째(반복기의 위치)값이 된다. 그 후 인덱스 +1
+                    return true;        // 참 반환
+                }
+                else        // 그 외의 경우 = 반복기가 리스트를 다 돌고 끝을 넘어간경우
+                {
+                    current = default(T);       // 현재값을 기본값으로
+                    index = list.Count;     // 반복기를 제일 뒤로
+                    return false;       // 거짓 반환
+                }
+            }
+
+            public void Reset()     // 초기화
+            {
+                index = 0;      // 반복기 위치를 처음으로 초기회
+                current = default(T);       // 현재값을 기본값으로
+            }
+        }
+        public int Average(List<int> list)
+        {
+            int sum = 0;
+            int ave = 0;
+            foreach (int i in list) { sum = sum + i; }
+            ave = sum / list.Count;
+            return ave;
+        }
+    }
+}
